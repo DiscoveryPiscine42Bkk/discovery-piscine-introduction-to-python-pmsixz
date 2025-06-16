2b63a6c904d10108c63945688a21196b3aedaaad
pmsixz <pmxicz@gmail.com>
2025-06-16 15:19:46 +0700

    Delete cell03/ex01/ex02 directory

--- cell03/ex01/ex02/i_got_that.py
+++
@@ -1,8 +0,0 @@
-while True:
-  user_input = input("Say something (type 'STOP' to exit): ")
-
-if user(input.upper( ) == "STOP":
-  print("Stopping the program.")
-  break
-
-print(f"I got that: {user_input}")
