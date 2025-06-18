e473a7219946d4f8b7daca8ddfdf347aabe5af1f
pmsixz <pmxicz@gmail.com>
2025-06-18 08:03:14 +0000

    good boy

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,6 +1,8 @@
-import sys
 def main():
-    num_params = len(sys.argv) - 1
+    user_input = input("Please enter some parameters (separate by space): ")
+
+    params = user_input.split()
+    num_params = len(params)
     print(f"Number of parameters: {num_params}.")
 
 if __name__ == "__main__":
---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) != 2:
+    input_string = input("Please enter a string to check for 'z': ")
+else:
+    input_string = sys.argv[1]
+
+z_count = input_string.count('z')
+
+# แสดงผล
+if z_count > 0:
+    print('z' * z_count)
+else:
+    print("none")
